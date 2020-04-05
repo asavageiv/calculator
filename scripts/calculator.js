235710a4b2901c0c64bfd2ddb4d3ee53c7b062b8
Alan Savage <asavageiv@gmail.com>
2020-04-05 18:34:08 -0400

    Fix operator precedence

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -15,15 +15,12 @@ const divide = (a, b) => {
 };
 
 const Operation = Object.freeze({
-  ADD: {apply: add, precedence: 0},
-  SUBTRACT: {apply: subtract, precedence: 0},
-  MULTIPLY: {apply: multiply, precedence: 1},
-  DIVIDE: {apply: divide, precedence: 1},
+  EQUALS: {apply: (a) => a, precedence: 0, unary: true},
+  ADD: {apply: add, precedence: 1},
+  SUBTRACT: {apply: subtract, precedence: 1},
+  MULTIPLY: {apply: multiply, precedence: 2},
+  DIVIDE: {apply: divide, precedence: 2},
 });
-
-
-const OP_ORDER = Object.freeze(
-    [Operation.MULTIPLY, Operation.DIVIDE, Operation.ADD, Operation.SUBTRACT]);
 
 class Calculator {
   constructor() {
@@ -60,7 +57,6 @@ class Calculator {
    * @param {Operation} operation
    */
   inputOperation(operation) {
-    console.log('operation', operation, this.inputs);
     // Ignore operators entered before any input
     if (this.inputs.length === 0 && this.isStartingNewNumber) {
       return;
@@ -71,54 +67,51 @@ class Calculator {
       this.inputs.pop();
       this.inputs.push(operation);
       this.subtotal();
-      console.log(this.inputs);
       return;
     }
     this.inputs.push(Number(this.display));
     this.inputs.push(operation);
     this.subtotal();
     this.isStartingNewNumber = true;
-    console.log('end operation', this.inputs);
   }
 
   subtotal() {
-    // If we don't have 4 inputs then we have at most one operation in the
-    // queue with 2 operands.
-    if (this.inputs.length < 4) {
+    // There are 3 cases to handle. Nothing in the input, 2 things in the input
+    // with a unary operator, and 4 or more things in the input.
+    if (this.inputs.length == 0) {
       return;
     }
-    // const currentOp = this.inputs.pop();
-
-    // Get the current operator
-    const newestOp = this.inputs[this.inputs.length - 1];
-    // Find the previous operator, abort if there isn't one
-    const prevOp = this.inputs[this.inputs.length - 3];
-    // If the previous operator has greater or equal precedence then we are safe
-    // to execute it with its operands and replace it with its result.
-    if (prevOp.precedence >= newestOp.precedence) {
-      const a = this.inputs[this.inputs.length - 4];
-      const b = this.inputs[this.inputs.length - 2];
-      const result = prevOp.apply(a, b);
-      this.inputs.splice(this.inputs.length - 4, 3, result);
-      this.display = '' + result;
-    }
+    let initialLength;
+    do {
+      initialLength = this.inputs.length;
+      const newestOp = this.inputs[this.inputs.length - 1];
+      if (this.inputs.length == 2 && newestOp === Operation.EQUALS) {
+        this.display = '' + newestOp.apply(this.inputs[0]);
+        this.inputs.pop();
+        this.inputs.pop();
+      } else if (this.inputs.length > 2) {
+        console.assert(this.inputs.length >= 4);
+        const prevOp = this.inputs[this.inputs.length - 3];
+        // If the previous operator has greater or equal precedence then we are
+        // safe to execute it with its operands and replace it with its result.
+        // Note that if the newest operator is "EQUALS", everything has greater
+        // precedence so it will all be evaluated until we hit the case above.
+        if (prevOp.precedence >= newestOp.precedence) {
+          const a = this.inputs[this.inputs.length - 4];
+          const b = this.inputs[this.inputs.length - 2];
+          const result = prevOp.apply(a, b);
+          this.inputs.splice(this.inputs.length - 4, 3, result);
+          this.display = '' + result;
+        }
+      }
+    } while (initialLength !== this.inputs.length);
   }
 
   equals() {
     // TODO: check that the input is ready to be calculated
     this.inputs.push(Number(this.display));
-    // TODO Ops of same precedence go LTR.
-    for (const operator of OP_ORDER) {
-      let operatorIndex = this.inputs.indexOf(operator);
-      while (operatorIndex > -1) {
-        const a = this.inputs[operatorIndex - 1];
-        const b = this.inputs[operatorIndex + 1];
-        const result = operator.apply(a, b);
-        this.inputs.splice(operatorIndex - 1, 3, result);
-        operatorIndex = this.inputs.indexOf(operator);
-      }
-    }
-    this.display = '' + this.inputs.pop();
+    this.inputs.push(Operation.EQUALS);
+    this.subtotal();
     this.isStartingNewNumber = true;
   }
 
--- spec/calculator.spec.js
+++ spec/calculator.spec.js
@@ -1,8 +1,7 @@
 import {Calculator, Operation} from '../scripts/calculator.js';
 
 describe('Calculator', () => {
-  const near = 
-  it('2+2=4', () => {
+  const near = it('2+2=4', () => {
     const calc = new Calculator();
     calc.inputDigit('2');
     calc.inputOperation(Operation.ADD);
@@ -112,5 +111,52 @@ describe('Calculator', () => {
         return Math.abs(Number(actual) - 4.3) < .01;
       },
     });
+  });
+
+  it('4/2*8=16', () => {
+    const calc = new Calculator();
+    calc.inputDigit('4');
+    calc.inputOperation(Operation.DIVIDE);
+    calc.inputDigit('2');
+    calc.inputOperation(Operation.MULTIPLY);
+
+    expect(calc.getDisplay()).toEqual('2');
+
+    calc.inputDigit('8');
+    calc.equals();
+
+    expect(calc.getDisplay()).toEqual('16');
+  });
+
+  it('4-2*16/8+32/2=16', () => {
+    const calc = new Calculator();
+    calc.inputDigit('4');
+    calc.inputOperation(Operation.SUBTRACT);
+    calc.inputDigit('2');
+    calc.inputOperation(Operation.MULTIPLY); // 4-2*
+
+    expect(calc.getDisplay()).toEqual('2');
+
+    calc.inputDigit('1');
+    calc.inputDigit('6');
+    calc.inputOperation(Operation.DIVIDE); // 4-2*16/ -> 4-32/
+
+    expect(calc.getDisplay()).toEqual('32');
+
+    calc.inputDigit('8');
+    calc.inputOperation(Operation.ADD); // 4-32/8+ -> 4-4+ -> 0+
+
+    expect(calc.getDisplay()).toEqual('0');
+
+    calc.inputDigit('3');
+    calc.inputDigit('2');
+    calc.inputOperation(Operation.DIVIDE); // 0+32/
+
+    expect(calc.getDisplay()).toEqual('32');
+
+    calc.inputDigit('2');
+    calc.equals(); // 0+32/2= -> 0+16= -> 16= -> '16'
+
+    expect(calc.getDisplay()).toEqual('16');
   });
 });
